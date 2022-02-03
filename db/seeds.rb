68147af1fbc7f72927ca0046e838bb122cf102b9
Lucia Moral <lmoralbaena@gmail.com>
2022-02-03 20:53:38 +0100

    added cloudinary gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "clean DB"
+Driver.destroy_all
 
+puts "create at least 3 drivers"
+
+Driver.create!(
+  name: "John Smith",
+  license_type: "car",
+  years_driving: 10,
+  transmission: "manual",
+  location: "Europe",
+  price:
+  photo:
+ )
+
+ Driver.create!(
+  name: "Charlotte Milliken",
+  license_type: "car",
+  years_driving: 17,
+  transmission: "automatic",
+  location: "Europe",
+  price:
+  photo:
+ )
+
+ Driver.create!(
+  name: "Tobias Weber",
+  license_type: "HGV",
+  years_driving: 25,
+  transmission: "manual",
+  location: "Germany",
+  price:
+  photo:
+ )
+
+ require "open-uri"
