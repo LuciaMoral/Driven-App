4c7fa6b0f91c94e3ebb38e888838ad0b8d870499
Lucia Moral <lmoralbaena@gmail.com>
2022-02-04 14:53:27 +0100

    added associations to models

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,6 @@ class Driver < ApplicationRecord
   validates :license_type, :years_driving, :transmission, :location, :price, uniqueness: true
   validates :name, presence: true
   has_one_attached :photo
+
+  has_many :bookings
 end
