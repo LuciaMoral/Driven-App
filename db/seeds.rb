f90a90c9df60b75cfa359a1a72ed2e05c10e914b
Lucia Moral <lmoralbaena@gmail.com>
2022-02-12 14:23:50 +0100

    re-seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,42 +14,50 @@ Driver.destroy_all
 
 puts "create at least 10 drivers"
 
-pictures = [
+female_pictures = [
   'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651886/szvoreny-attila-Z_DjhwOUvTw-unsplash_b1tcga.jpg',
-  'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651616/linkedin-sales-solutions-pAtA8xe_iVM-unsplash_wgrejc.jpg',
-  'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651541/ali-morshedlou-WMD64tMfc4k-unsplash_pffbmq.jpg',
   'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651451/jake-nackos-IF9TK5Uy-KI-unsplash_pwdezk.jpg',
   'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651375/christina-wocintechchat-com-SJvDxw0azqw-unsplash_b8zbz9.jpg',
   'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651375/christina-wocintechchat-com-Kpd-JMLeKJk-unsplash_itrnxp.jpg',
-  'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651375/jurica-koletic-7YVZYZeITc8-unsplash_ak3fba.jpg',
-  'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651375/ben-parker-OhKElOkQ3RE-unsplash_npnqtl.jpg',
-  'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651375/joseph-gonzalez-iFgRcqHznqg-unsplash_ew5zzt.jpg',
   'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651375/edward-cisneros-_H6wpor9mjs-unsplash_ul5a2w.jpg'
 ]
+male_pictures = [
+  'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651616/linkedin-sales-solutions-pAtA8xe_iVM-unsplash_wgrejc.jpg',
+  'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651541/ali-morshedlou-WMD64tMfc4k-unsplash_pffbmq.jpg',
+  'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651375/jurica-koletic-7YVZYZeITc8-unsplash_ak3fba.jpg',
+  'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651375/joseph-gonzalez-iFgRcqHznqg-unsplash_ew5zzt.jpg',
+  'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651375/ben-parker-OhKElOkQ3RE-unsplash_npnqtl.jpg'
+]
 
-pictures.each do |picture_url|
-  file = URI.open(picture_url)
-  driver = Driver.new(
-    name: Faker::Name.name,
+female_pictures.each do |female_picture_url|
+  file = URI.open(female_picture_url)
+  female_driver = Driver.new(
+    name: Faker::Name.female_first_name,
     license_type: ["A", "B", "C"].sample,
-    years_driving: rand(1..50),
+    years_driving: rand(1..30),
     transmission: ["auto", "manual"].sample,
-    location: Faker::Address.full_address,
+    location: ["London", "Shoreditch", "Brixton", "Hampstead"].sample,
     price: rand(10..50)
   )
-  driver.photo.attach(io: file, filename: picture_url, content_type: 'image/jpg')
-  driver.save!
+  female_driver.photo.attach(io: file, filename: female_picture_url, content_type: 'image/jpg')
+  female_driver.save!
 end
 
-  # Driver.create!(
-  #   name: Faker::Name.name,
-  #   license_type: ["A", "B", "C"].sample,
-  #   years_driving: rand(1..50),
-  #   transmission: ["auto", "manual"].sample,
-  #   location: Faker::Address.full_address,
-  #   price: rand(10..50),
-  #   photo: picture
-  # )
+male_pictures.each do |male_picture_url|
+  file = URI.open(male_picture_url)
+  male_driver = Driver.new(
+    name: Faker::Name.male_first_name,
+    license_type: ["A", "B", "C"].sample,
+    years_driving: rand(1..30),
+    transmission: ["auto", "manual"].sample,
+    location: ["London", "Shoreditch", "Brixton", "Hampstead"].sample,
+    price: rand(10..50)
+  )
+  male_driver.photo.attach(io: file, filename: male_picture_url, content_type: 'image/jpg')
+  male_driver.save!
+end
+puts "10 new drivers created"
+
 # file = URI.open('https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80')
 # john = Driver.new(
 #   name: "John Smith",
@@ -85,5 +93,3 @@ end
 # )
 # tobias.photo.attach(io: file, filename: 'tobias.jpg', content_type: 'image/jpg')
 # tobias.save!
-
-puts "10 new drivers created"
